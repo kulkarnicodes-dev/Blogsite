b5d579ab6f529921783a83df7cb9f8bc219f6e42
Yash Kulkarni <156276524+kulkarnicodes-dev@users.noreply.github.com>
2026-07-18 11:20:18 +0530

    Modify build.sh for dependency installation and superuser
    Updated build script to install dependencies from requirements.txt and added superuser creation.

--- blogsite/build.sh
+++ blogsite/build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install uv
-uv sync
+pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python manage.py collectstatic --noinput
 python manage.py migrate
+
+python create_superuser.py
